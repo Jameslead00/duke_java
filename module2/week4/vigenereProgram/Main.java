2f1aba2bf020daccdd3c9f577d9085b5d3e28f18
jameslead <jleadbeater@student.ethz.ch>
2023-07-25 11:03:43 +0200

    written tests

---
+++ module2/week4/vigenereProgram/Main.java
@@ -0,0 +1,11 @@
+package duke_java.module2.week4.vigenereProgram;
+public class Main{
+    public static void main(String[] args){
+        Tester tester = new Tester();
+
+        //tester.testCaesarCipher();
+        //tester.testCaesarCracker();
+        tester.testVigenereCipher();
+
+    }
+}
